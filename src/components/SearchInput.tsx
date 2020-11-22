d14d2055350beae0b3f5eae8b8b081c77332cef3
Marton Dobos <dobeis2@gmail.com>
2020-11-22 12:24:24 +0100

    text fix

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,19 +1,19 @@
-import React, { useRef, useContext, MouseEvent } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import InputBase from "@material-ui/core/InputBase";
-import IconButton from "@material-ui/core/IconButton";
-import SearchIcon from "@material-ui/icons/Search";
+import React, { useRef, useContext, MouseEvent } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
 
-import { MediaContext } from "../context/MediaProvider";
+import { MediaContext } from '../context/MediaProvider';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 520,
-    padding: "2px 4px",
-    display: "flex",
-    alignItems: "center",
-    margin: "32px auto",
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    margin: '32px auto',
   },
   input: {
     marginLeft: theme.spacing(1),
@@ -32,22 +32,17 @@ const SearchInput = () => {
   const onSerachClick = (e: MouseEvent) => {
     e.preventDefault();
     fetchMedia({ variables: { query: inputRef.current.value } });
-  }
+  };
 
   return (
     <Paper component="form" className={classes.root}>
       <InputBase
         inputRef={inputRef}
         className={classes.input}
-        placeholder="Search for a movies and films"
-        inputProps={{ "aria-label": "search for movies and films" }}
+        placeholder="Search for movies and films"
+        inputProps={{ 'aria-label': 'search for movies and films' }}
       />
-      <IconButton
-        type="submit"
-        className={classes.iconButton}
-        aria-label="search"
-        onClick={onSerachClick}
-      >
+      <IconButton type="submit" className={classes.iconButton} aria-label="search" onClick={onSerachClick}>
         <SearchIcon />
       </IconButton>
     </Paper>
